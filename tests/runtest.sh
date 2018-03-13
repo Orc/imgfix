05566d36c58f59532631495ad8912599212484fe
david parsons <orc@pell.portland.or.us>
2018-03-12 21:39:32 -0700

    change the test harness to modify copies of the test file and compare them to the expected output

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -4,10 +4,16 @@ cd tests || exit 1
 for x in *.test; do
     result=`basename $x .test`.done
     if [ -r $result ]; then
+	cp $x /tmp/$$.test
 	printf "%s: "  `basename $x .test`
-	../imgfix < $x | diff - $result 2>/dev/null 1>&2 \
+	if ../imgfix /tmp/$$.test; then
+	    diff /tmp/$$.test  $result 2>/dev/null 1>&2 \
 		&& echo ok \
 		|| echo failed
+	else
+	    echo failed
+	fi
+	rm /tmp/$$.test
     fi
 done
 exit 0
