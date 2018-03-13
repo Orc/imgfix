92856751b32d4b38b96e9a28e9fc8b2ba78399e1
david parsons <orc@pell.portland.or.us>
2018-03-12 17:05:48 -0700

    add two more tests

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -3,6 +3,11 @@
 cd tests || exit 1
 for x in *.test; do
     result=`basename $x .test`.done
-    printf "%s: "  `basename $x .test`; ../imgfix < $x | diff - $result && echo ok || echo failed
+    if [ -r $result ]; then
+	printf "%s: "  `basename $x .test`
+	../imgfix < $x | diff - $result 2>/dev/null 1>&2 \
+		&& echo ok \
+		|| echo failed
+    fi
 done
 exit 0
